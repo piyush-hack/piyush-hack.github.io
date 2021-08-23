778ee9fecc4fb74d3d9295a21fdd99ad2e2bc728
piyush-hack <75742731+piyush-hack@users.noreply.github.com>
2021-08-23 19:51:39 +0530

    Create includeprojects.js

---
+++ mysecwebpage/static/includeprojects.js
@@ -0,0 +1,11 @@
+$(function () {
+  $("#projects").load("myprojects.html");
+});
+
+$(function () {
+  $("#internships").load("myinternships.html");
+});
+
+$(function () {
+  $("#skills").load("myskills.html");
+});
